74977699402a03a429d30a12c43deae60b4b6775
Gustav André Bilben <gustav.bilben@gmail.com>
2015-10-28 23:13:46 +0100

    initial commit

---
+++ test/eratosthenes_sieve/core_test.clj
@@ -0,0 +1,7 @@
+(ns eratosthenes-sieve.core-test
+  (:require [clojure.test :refer :all]
+            [eratosthenes-sieve.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
