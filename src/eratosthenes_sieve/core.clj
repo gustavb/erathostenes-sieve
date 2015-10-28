e441fe1f11c6510a9630938ecf19971ed79abc20
Gustav André Bilben <gustav.bilben@gmail.com>
2015-10-28 23:47:05 +0100

    refactored code

--- src/eratosthenes_sieve/core.clj
+++ src/eratosthenes_sieve/core.clj
@@ -1,5 +1,12 @@
 (ns eratosthenes-sieve.core
   (:gen-class))
 
+(defn- integers-up-to [n]
+  (range 2 (inc n)))
+
+(defn- multiple-of? [n p]
+  (and (= (mod n p) 0) (not= n p)))
+
 (defn primes-up-to [n]
-  (filter #(or (not= (mod % 2) 0) (= % 2)) (range 2 (inc n))))
+  (remove #((multiple-of? % 2))
+          (integers-up-to n)))
